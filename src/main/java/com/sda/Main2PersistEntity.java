a087e3a95cdbec8b6b0e69cf4b9ecd470b3d8b40
Michal Paukszto <michal.paukszto@gmail.com>
2022-07-09 11:50:39 +0200

    persist

--- src/main/java/com/sda/Main2PersistEntity.java
+++ src/main/java/com/sda/Main2PersistEntity.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-public class Main2 {
+public class Main2PersistEntity {
 
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
@@ -13,7 +13,12 @@ public class Main2 {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        // todo ....
+        session.persist(new Dog("Ares", 10, "Terier"));
+        session.persist(new Dog("Sonia", 8, "Buldog"));
+        session.persist(new Dog("Reksio", 11, "Doberman"));
+        session.persist(new Dog("Benio", 7, "Jamink"));
+        session.persist(new Dog("Aza", 12, "Wilczur"));
+        session.persist(new Dog("Pimpek", 5, "Terier"));
 
         transaction.commit();
         session.close();
--- src/main/java/com/sda/entity/Dog.java
+++ src/main/java/com/sda/entity/Dog.java
@@ -4,15 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "dog")
 public class Dog {
 
     @Id
