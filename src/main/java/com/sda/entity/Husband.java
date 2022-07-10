db2a13a8300025600963674051750e1b2137d793
Michal Paukszto <michal.paukszto@gmail.com>
2022-07-10 11:50:13 +0200

    one to one remove cascade

--- src/main/java/com/sda/entity/Husband.java
+++ src/main/java/com/sda/entity/Husband.java
@@ -17,7 +17,7 @@ public class Husband {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String name;
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     Wife wife; // na podstawie tego pola jest mapowana relacja
 
     public Husband(String name) {
